ed053643a13ecb31f3015869d89120cf27870759
scan-vidhaan <84076117+scan-vidhaan@users.noreply.github.com>
2021-08-28 14:15:45 +0530

    Update Activity_04.py

---
+++ Activity_04.py
@@ -0,0 +1,10 @@
+#input
+vidhaan= int(input("enter the first number"))
+appaji= int(input("enter the second number"))
+thug= vidhaan + appaji
+print(thug)
+#output
+enter the first number 20
+enter the second number 60
+80
+>>> 
