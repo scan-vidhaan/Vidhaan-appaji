469ba4372ebc9fbd0da81304fe9166046ac69df0
scan-vidhaan <84076117+scan-vidhaan@users.noreply.github.com>
2021-08-28 13:55:47 +0530

    Update Activity_03.py

---
+++ Activity_03.py
@@ -0,0 +1,17 @@
+#input 
+string1= input("enter the first string \n")
+string2= input("enter the second string \n")
+ss = string1+string2
+
+print(ss*5)
+
+
+
+#output
+enter the first string 
+good
+enter the second string 
+morning
+goodmorninggoodmorninggoodmorninggoodmorninggoodmorning
+>>> 
+
